f61e2cfcc2d41d03fdd5c52d26666ad81c75f18a
Priyanka250602 <161591758+Priyanka250602@users.noreply.github.com>
2024-08-27 18:35:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Flipcoin from './Flipcoin';
+
+function App() {
+  return (
+    <div className="App">
+      <Flipcoin />
+    </div>
+  );
+}
+
+export default App;
